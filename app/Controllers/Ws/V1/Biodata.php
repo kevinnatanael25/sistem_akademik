3a9ef69ace1f52f674bbfa49468a45e437c00d86
Kevin Alfiah <natanael.kevin01@gmail.com>
2021-11-01 16:06:06 +0900

    as

--- app/Controllers/Ws/V1/Biodata.php
+++ app/Controllers/Ws/V1/Biodata.php
@@ -19,14 +19,12 @@ class Biodata extends ResourceController
     {
 
         $token = $this->session->get('token');
-
-        
-
     }
     public function read()
     {
         $id_mahasiswa = $this->request->getGet('id_mahasiswa');
-        $item = $this->rest->callRest("GetListMahasiswa", $this->session->get('token'), "id_mahasiswa=".'$id_mahasiswa', '');
-        return $this->respond($item);
+        // $item = $this->rest->callRest("GetListMahasiswa", $this->session->get('token'), "id_mahasiswa=".'$id_mahasiswa', '');
+
+        return $this->respond($id_mahasiswa);
     }
 }
