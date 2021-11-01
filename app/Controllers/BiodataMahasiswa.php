18bd05355d0ee68f8674acb256e10b4f3499d6d2
Kevin Alfiah <natanael.kevin01@gmail.com>
2021-11-01 15:34:35 +0900

    github

---
+++ app/Controllers/BiodataMahasiswa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BiodataMahasiswa extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
--- app/Models/PerguruanTinggiModel.php
+++ app/Models/PerguruanTinggiModel.php
@@ -5,17 +5,20 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 
-class ProdiModel extends Model
+class PerguruanTinggiModel extends Model
 {
     protected $DBGroup              = 'default';
-    protected $table                = 'program_studi';
+    protected $table                = 'perguruan_tinggi';
     protected $primaryKey           = 'id';
     protected $useAutoIncrement     = true;
     protected $insertID             = 0;
     protected $returnType           = 'array';
     protected $useSoftDeletes       = false;
     protected $protectFields        = true;
-    protected $allowedFields        = ['id','kode_program_studi','nama_program_studi','status','id_jenjang_pendidikan'];
+    protected $allowedFields        = ['id','kode_perguruan_tinggi', 'nama_perguruan_tinggi','telepon','faximile','email',
+                                        'website','jalan', 'dusun', 'rt_rw', 'kelurahan', 'kode_pos', 'bank', 'unit_cabang',
+                                        'nomor_rekening', 'mbs', 'luas_tanah_milik','luas_tanah_bukan_milik','sk_pendirian',
+                                        'tanggal_sk_pendirian', 'status_perguruan_tinggi', 'sk_izin_operasional', 'tanggal_izin_operasional'];
 
     // Dates
     protected $useTimestamps        = false;
