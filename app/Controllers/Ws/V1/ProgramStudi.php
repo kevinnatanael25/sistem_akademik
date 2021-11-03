c222f9cb5ec7dfba98c9e21815fd224b3a0b8b5e
Kevin Alfiah <natanael.kevin01@gmail.com>
2021-11-03 18:43:27 +0900

    dfgd

--- app/Controllers/Ws/V1/ProgramStudi.php
+++ app/Controllers/Ws/V1/ProgramStudi.php
@@ -17,18 +17,18 @@ class ProgramStudi extends ResourceController
         $this->session = session();
         $this->rest = new Rest();
     }
-  
+    
     public function read()
     {
         $item = $this->rest->callRest("GetAllProdi", $this->session->get('token'), "id_perguruan_tinggi='14a2759a-ce83-48eb-85c9-db4f8de00f20' AND status='A'",'');
         return $this->respond($item);
     }
-    public function add()
-    {
+    // public function add()
+    // {
         
-        $item = $this->rest->callRest("GetListMahasiswa", $this->session->get('token'), "nama_status_mahasiswa = 'Aktif'", '');
+    //     $item = $this->rest->callRest("GetListMahasiswa", $this->session->get('token'), "nama_status_mahasiswa = 'Aktif'", '');
 
-        $result = $this->model->insertBatch($item->data);
-        return $this->respond($result);
-    }
+    //     $result = $this->model->insertBatch($item->data);
+    //     return $this->respond($result);
+    // }
 }
